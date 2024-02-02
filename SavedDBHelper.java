5833f34f43923b98ec4342303ac52fce3f3c1a1b
dyoon-23 <150774513+dyoon-23@users.noreply.github.com>
2024-02-02 09:24:46 +0900

    Create SavedDBHelper.java

---
+++ SavedDBHelper.java
@@ -0,0 +1,28 @@
+package com.example.beepme;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SavedDBHelper extends SQLiteOpenHelper {
+    public SavedDBHelper(Context context) {
+        super(context, "savedDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE savedTBL(barNum INTEGER, Name CHAR(20) PRIMARY KEY, allergy CHAR(20), nutrient CHAR(20), manufacture CHAR(20), source CHAR(20));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS savedTBL");
+        onCreate(db);
+    }
+
+    public void resetData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM savedTBL");
+        db.close();
+    }
+}
